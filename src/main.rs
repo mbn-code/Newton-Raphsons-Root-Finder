6cbf8022771bb775c84d07177114f696c78ae6ad
mbn <66748817+CollinEdward@users.noreply.github.com>
2024-01-30 22:27:05 +0100

    update to use ggez graphics crate.

--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,42 @@
-use eframe::egui;
 
-// Function to calculate Fibonacci number
-fn fib(n: i32) -> i32 {
-    if n <= 1 {
-        n
-    } else {
-        fib(n - 1) + fib(n - 2)
+use ggez::event;
+use ggez::graphics::{self};
+use ggez::{Context, GameResult};
+use ggez::glam::*;
+
+struct MainState {
+    pos_x: f32,
+}
+
+impl MainState {
+    fn new() -> GameResult<MainState> {
+        let s = MainState { pos_x: 0.0 };
+        Ok(s)
     }
 }
 
-fn main() {
-    // Set up native options
-    let native_options = eframe::NativeOptions::default();
+impl event::EventHandler<ggez::GameError> for MainState {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+        self.pos_x = self.pos_x % 800.0 + 1.0;
+        Ok(())
+    }
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let canvas = graphics::Canvas::from_frame(
+            ctx,
+            graphics::Color::from([1.0,1.0,1.0,0.0]),
+        );
+
+ 
+        canvas.finish(ctx)?;
+        Ok(())
+    }
+}
+
+pub fn main() -> GameResult {
+    let cb = ggez::ContextBuilder::new("super_simple", "mbn");
+    let (ctx, event_loop) = cb.build()?;
+    let state = MainState::new()?;
+    event::run(ctx, event_loop, state)
+}
     
-    // Run the native eframe app
-    eframe::run_native("My egui App", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
-}
-
-#[derive(Default)]
-struct MyEguiApp {}
-
-impl MyEguiApp {
-    // Create a new instance of MyEguiApp
-    fn new(_cc: &eframe::CreationContext<'_>) -> Self {
-        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
-        // Restore app state using cc.storage (requires the "persistence" feature).
-        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
-        // for e.g. egui::PaintCallback.
-        Self::default()
-    }
-}
-
-impl eframe::App for MyEguiApp {
-    // Update method for the eframe app
-    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        // Show the central panel
-        egui::CentralPanel::default().show(ctx, |ui| {
-            // Display Fibonacci numbers in a loop
-            for i in 0..=20 {
-                ui.label(format!("fib({}) = {}", i, fib(i)));
-            }
-        });
-    }
-}
