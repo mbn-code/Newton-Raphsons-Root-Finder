068bbf16945a873a08e492a10cc7b22b0125e4ce
mbn <66748817+CollinEdward@users.noreply.github.com>
2024-01-30 22:10:01 +0100

    working egui app

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use eframe::egui;
 
+// Function to calculate Fibonacci number
 fn fib(n: i32) -> i32 {
     if n <= 1 {
-        1
+        n
     } else {
         fib(n - 1) + fib(n - 2)
     }
 }
 
-
 fn main() {
+    // Set up native options
     let native_options = eframe::NativeOptions::default();
+    
+    // Run the native eframe app
     eframe::run_native("My egui App", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
 }
 
@@ -18,27 +21,25 @@ fn main() {
 struct MyEguiApp {}
 
 impl MyEguiApp {
+    // Create a new instance of MyEguiApp
     fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
         // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
         // for e.g. egui::PaintCallback.
-        
         Self::default()
     }
 }
 
 impl eframe::App for MyEguiApp {
+    // Update method for the eframe app
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-       egui::CentralPanel::default().show(ctx, |ui| {
-
-
+        // Show the central panel
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // Display Fibonacci numbers in a loop
             for i in 0..=20 {
                 ui.label(format!("fib({}) = {}", i, fib(i)));
             }
-
-
-
         });
     }
 }
