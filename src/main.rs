f8079f12701c248e20fa09f2901e1562e0fa861c
mbn <66748817+CollinEdward@users.noreply.github.com>
2024-02-03 21:30:46 +0100

    Add mint dependency for graphics

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use ggez::{conf, event, graphics, Context, GameResult};
 use lazy_static::lazy_static;
-use ggez::input::keyboard::KeyCode;
+
+// For keyboard input use
+// use ggez::input::keyboard::KeyCode;
 
 // declaration and initialization of constant global variables
 lazy_static! {
@@ -8,65 +10,47 @@ lazy_static! {
     static ref SCREEN_HEIGHT: f32 = 720.0;
 }
 
-
 struct MainState {
-    pos_x: f32,
-    pos_y: f32,
+    _pos_x: f32,
+    _pos_y: f32,
 }
 
 impl MainState {
     fn new() -> GameResult<MainState> {
-        let s = MainState { pos_x: 0.0 , pos_y: 0.0};
+        let s = MainState { _pos_x: 0.0 , _pos_y: 0.0};
         Ok(s)
     }
 }
 
+fn fibonacci(n: u32) -> u32 {
+    
+    let mut _arr: Vec<u32> = Vec::new();
+
+
+    if n == 0 {
+        return 0;
+    }
+    if n == 1 {
+        return 1;
+    }
+
+    return fibonacci(n - 1) + fibonacci(n - 2);
+}
+
 impl event::EventHandler<ggez::GameError> for MainState {
-    fn update(&mut self, ctx: &mut Context) -> GameResult {
-
-        if ctx.keyboard.is_key_pressed(KeyCode::Right) {
-            // move the rectangle to the right
-            self.pos_x += 10.0;
-        }
-        if ctx.keyboard.is_key_pressed(KeyCode::Left) {
-            // move the rectangle to the left
-            self.pos_x -= 10.0;
-
-        }
-        if ctx.keyboard.is_key_pressed(KeyCode::Up) {
-            // move the rectangle to the up
-            self.pos_y -= 10.0;
-
-        }
-        if ctx.keyboard.is_key_pressed(KeyCode::Down) {
-            // move the rectangle to the down
-            self.pos_y += 10.0;
-        }
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
 
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        let mut canvas =
+        let canvas =
             graphics::Canvas::from_frame(ctx, graphics::Color::from([1.0, 1.0, 1.0, 0.0]));
 
-        // draw a rectangle in the middle of the screen
-        let rect_length = 100.0;
-        let rect_height = 100.0;
 
-        let rect: graphics::Mesh = graphics::Mesh::new_rectangle(
-            ctx,
-            graphics::DrawMode::fill(),
-            graphics::Rect::new(
-                self.pos_x,
-                self.pos_y,
-                rect_length,
-                rect_height,
-            ),
-            graphics::Color::from([0.0, 0.0, 0.0, 1.0]),
-        )?;
-        canvas.draw(&rect, graphics::DrawParam::default());
 
+
+            
         canvas.finish(ctx)?;
         Ok(())
     }
@@ -74,6 +58,8 @@ impl event::EventHandler<ggez::GameError> for MainState {
 
 pub fn main() -> GameResult {
     
+    fibonacci(10);
+
     // Create a context builder with window dimensions
     let cb = ggez::ContextBuilder::new("super_simple", "mbn")
         .window_setup(conf::WindowSetup::default().title("Your Game Title"))
