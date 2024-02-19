411bf5fcf5b60836993d1f2d7572a2852a48dd68
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2024-02-19 11:44:51 +0100

    Update canvas size and animation in Newton's method

--- src_js/newton.js
+++ src_js/newton.js
@@ -7,7 +7,7 @@ let nextX;  // Next guess
 let coefficients;  // Coefficients of the polynomial function
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(1000, 1000);
     frameRate(240);  // Increase frame rate for smoother animation
     coefficients = new Array(3).fill().map(() => random(-1, 1));  // Generate random coefficients
 }
@@ -15,26 +15,14 @@ function setup() {
 let scale = 100;  // Scale of the graph
 
 function draw() {
-
     textSize(16);
     fill('black');
-    text('Root: ' + x.toFixed(2), 10, 20);
 
-    let hasRoot = false;
-    for (let x = -2; x <= 2; x += 0.01) {
-        let y = f(x);
-        if (abs(y) < precision) {
-            hasRoot = true;
-            break;
-        }
+
+    if (!hasRoot()) {
+        generateGraph();
     }
 
-    if (!hasRoot) {
-        // If there is no root, generate new random coefficients
-        coefficients = new Array(3).fill().map(() => random(-1, 1));
-    }
-    
-    
     background(255);
     // Draw axes
     stroke('black');
@@ -59,25 +47,58 @@ function draw() {
     line(x * scale + width / 2, height / 2 - y * scale, (x - y / slope) * scale + width / 2, height / 2);
     stroke('black');
     // Animate the movement of the guess
+    fill('blue');
+    ellipse(x * scale + width / 2, height / 2 - f(x) * scale, 10);
+    fill('green');
+    ellipse(nextX * scale + width / 2, height / 2 - f(nextX) * scale, 10);
+
+    // draw a line from the end of the red line to the x axies to show where it's going to be
+    stroke('red');
+    line((x - y / slope) * scale + width / 2, height / 2, (x - y / slope) * scale + width / 2, height / 2 - y * scale);
+    // Draw the x axis
+    stroke('black');
+    line(0, height / 2 - y * scale, (x - y / slope) * scale + width / 2, height / 2 - y * scale);
+    // Draw the y axis
+    line((x - y / slope) * scale + width / 2, height / 2 - y * scale, (x - y / slope) * scale + width / 2, height / 2);
+   
+
+    // Draw the x value
+    text(x.toFixed(5), (x - y / slope) * scale + width / 2, height / 2 + 15);
+    // Draw the y value
+    text(y.toFixed(5), 5, (height / 2 - y * scale).toFixed(4));
+    
+
+
+    // Animate the movement of the guess
     if (animate) {
-        x = lerp(x, nextX, 0.05);
+        x = lerp(x, nextX, 0.01);  // Slow down the animation
         if (abs(x - nextX) < 0.01) {
             animate = false;
         }
     } else {
         // Apply Newton's method
+        let y = f(x);
+        let slope = df(x);
         nextX = x - y / slope;
         animate = true;
         iterations++;
-        if (isNaN(y) || !isFinite(y) || abs(y) < precision || iterations > maxIterations) {
-            x = random(-2, 2);  // Reset initial guess
-            iterations = 0;  // Reset number of iterations
-            coefficients = new Array(3).fill().map(() => random(-1, 1));  // Generate new random coefficients
-        }
     }
+    text('Iterations: ' + iterations, 10, 30);
 }
 
+function hasRoot() {
+    for (let x = -2; x <= 2; x += 0.01) {
+        let y = f(x);
+        if (abs(y) < precision) {
+            return true;
+        }
+    }
+    return false;
+}
 
+function generateGraph() {
+    coefficients = new Array(3).fill().map(() => random(-1, 1));
+}
 
 // Function for which to find root
 function f(x) {
