051137b86397998ef8b5752d78ce02e67c8789ff
1cy50ng3b0b <66748817+CollinEdward@users.noreply.github.com>
2024-02-21 11:22:19 +0100

    javascript done

--- src_js/newton.js
+++ src_js/newton.js
@@ -59,7 +59,7 @@ let nextX; // Next guess
 let coefficients; // Coefficients of the polynomial function
 
 function setup() {
-  createCanvas(1000, 800);
+  createCanvas(800, 800);
   frameRate(120); // Increase frame rate for smoother animation
   coefficients = new Array(3).fill().map(() => random(-1, 1)); // Generate random coefficients
   // Add mousePressed event to set the initial guess and reset the number of iterations and the animation
--- src_js/newton_rec.js
+++ src_js/newton_rec.js
@@ -1,12 +1,23 @@
-function newtonMethod(f, fPrime, x0, epsilon) {
+function newtonMethod(f, fDerivative, x0, epsilon) {
   const fx = f(x0);
-  const fpx = fPrime(x0);
+  const fpx = fDerivative(x0);
   
   if (Math.abs(fx) < epsilon) {
+    console.log("Root found:", x0);
     return x0;
   }
   
   const x1 = x0 - fx / fpx;
   
-  return newtonMethod(f, fPrime, x1, epsilon);
+  console.log("Current guess:", x0);
+  console.log("New guess:", x1);
+  
+  return newtonMethod(f, fDerivative, x1, epsilon); // Recursive call
 }
+
+newtonMethod(
+  x => x**5+3.5*x**4-2.5*x**3-12.5*x**2+1.5*x+9,
+  x => 5*x**4+14*x**3-7.5*x**2-25*x+1.5,
+  4,
+  1e-10
+);
